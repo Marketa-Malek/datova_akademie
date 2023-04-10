0a3e80a7331ebe588028d083a1675a86790250ba
Marketa-Malek <127792417+Marketa-Malek@users.noreply.github.com>
2023-04-10 16:02:09 +0200

    project_SQL_secondary_final
    Dodatečná tabulka s s HDP, GINI koeficientem a populací dalších evropských států ve stejném období, jako primární přehled pro ČR.

---
+++ project_SQL_secondary_final.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE t_marketa_malek_project_SQL_secondary_final
+SELECT 
+	c.continent,
+	e.country,
+	e.GDP,
+	e.gini,
+	e.population,
+	e.`year`
+FROM economies e 
+JOIN countries c
+	ON e.country = c.country
+WHERE c.continent = 'europe'
+	AND e.`year` BETWEEN 2006 AND 2018
+
+
+
+
+
+
+
+
+
+
+
+
+
